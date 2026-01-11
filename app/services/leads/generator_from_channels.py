86562e9cb8d4e308b608da954f662627db3f1634
Anton Lukin <mininzidane@gmail.com>
2026-01-11 11:48:40 -0800

    fix(lead generator): fix for post id is not int

--- app/services/leads/generator_from_channels.py
+++ app/services/leads/generator_from_channels.py
@@ -116,7 +116,7 @@ class GeneratorFromChannels:
                     text = matched_message.get('text')
                     post_id = matched_message.get('id')
                     sender_name = matched_message.get('name')
-                    if not post_id:
+                    if not int(post_id):
                         logger.error(f"[GeneratorFromChannels::generate_from_telegram_channels][{bot_clients[0].get_name()}] No post_id in matched message: {matched_message}")
                         continue
 
